f190eba81333a55d34c677fa45d8a32546e9a517
Shidul Islam <sishahin093@gmail.com>
2022-05-23 22:45:18 +0600

    database connection

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middle ware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+// user name : fixits-repaire
+// pass : w-KN4N*P5p2a_VZ
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://fixits-repaire:w-KN4N*P5p2a_VZ@cluster0.2bt3n.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('connnecteeed database .')
+    // perform actions on the collection object
+    client.close();
+});
 
 
 app.get('/', (req, res) => {
