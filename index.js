239425cb651e3d745c6e42cd95318ac333a1adf7
Shidul Islam <sishahin093@gmail.com>
2022-05-24 00:40:13 +0600

    use token

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -14,16 +16,64 @@ app.use(express.json());
 // user name : fixits-repaire
 // pass : w-KN4N*P5p2a_VZ
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: `UnAuthorized access` });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://fixits-repaire:w-KN4N*P5p2a_VZ@cluster0.2bt3n.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2bt3n.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    console.log('connnecteeed database .')
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+        client.connect();
+        const fixitsCollection = client.db("fixits-repaire").collection("services");
+        const userCollection = client.db("doctors_portal").collection("users");
+        // api data load
+        app.get('/tools', async (req, res) => {
+            const query = {};
+            const cursor = fixitsCollection.find(query);
+            const fasion = await cursor.toArray();
+            res.send(fasion);
+        });
+
+
+        // user update and token get
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const user = req.body;
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ result, token });
+        });
+
+        app.get('/user', verifyJWT, async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+
+        });
+
+    } finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
